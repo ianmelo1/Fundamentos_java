4534f1bbebc700316936ef64eb813c22ca11762d
ian melo gonçalves <160737561+ianmelo1@users.noreply.github.com>
2025-08-20 11:28:24 -0300

    Update Scanner_exemplos.java
    Uso do método split ultilizado

--- src/Scanner_exemplos.java
+++ src/Scanner_exemplos.java
@@ -4,20 +4,24 @@ public class Scanner_exemplos {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    System.out.print("Digite um número inteiro: ");
-    int num = sc.nextInt();
+    System.out.print("Digite um número inteiro, um número com virgula e um nome composto em série: ");
+    String texto= sc.nextLine();
 
-    System.out.print("Digite um numero com vírgula: ");
-    Double numDouble = sc.nextDouble();
-
-    sc.nextLine();
-
-    System.out.print("Digite um nome composto: ");
-    String nome = sc.nextLine();
-
-    System.out.println("O número inteiro que você digitou foi: " + num );
-    System.out.println("O número de ponto flutuante que você digitou foi: " + numDouble);
-    System.out.println("O nome composto que você digitou foi: " + nome);
+        String[] partes = texto.split(" ");
+        
+        int numInt = Integer.parseInt(partes[0]);
+      
+        double numDecimal = Double.parseDouble(partes[1].replace(",", "."));
+        
+        String nome = "";
+        for(int i = 2; i < partes.length; i++){
+            nome += partes[i];
+            if(i < partes.length - 1) nome += " ";
+        }
+        
+        System.out.println("Número inteiro: " + numInt);
+        System.out.println("Número decimal: " + numDecimal);
+        System.out.println("Nome composto: " + nome);
 
     sc.close();
 
