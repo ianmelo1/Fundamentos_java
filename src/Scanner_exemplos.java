27411f5bf0f87eae88747d67c316e88b6eafa08e
unknown <2412130071@ACADEMICO.IESB.BR>
2025-08-22 10:41:10 -0300

    Resolvendo o merge do Scanner_exemplos.java

--- src/Scanner_exemplos.java
+++ src/Scanner_exemplos.java
@@ -4,17 +4,6 @@ public class Scanner_exemplos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-<<<<<<< HEAD
-        System.out.print("Digite um número inteiro, um número com virgula e um nome composto em série: ");
-        String texto= sc.nextLine();
-
-        String[] partes = texto.split(" ");
-
-        int numInt = Integer.parseInt(partes[0]);
-
-        double numDecimal = Double.parseDouble(partes[1].replace(",", "."));
-
-=======
     System.out.print("Digite um número inteiro, um número com virgula e um nome composto em série: ");
     String texto= sc.nextLine();
 
@@ -24,19 +13,11 @@ public class Scanner_exemplos {
       
         double numDecimal = Double.parseDouble(partes[1].replace(",", "."));
         
->>>>>>> 4534f1bbebc700316936ef64eb813c22ca11762d
         String nome = "";
         for(int i = 2; i < partes.length; i++){
             nome += partes[i];
             if(i < partes.length - 1) nome += " ";
         }
-<<<<<<< HEAD
-=======
-        
-        System.out.println("Número inteiro: " + numInt);
-        System.out.println("Número decimal: " + numDecimal);
-        System.out.println("Nome composto: " + nome);
->>>>>>> 4534f1bbebc700316936ef64eb813c22ca11762d
 
         System.out.println("Número inteiro: " + numInt);
         System.out.println("Número decimal: " + numDecimal);
